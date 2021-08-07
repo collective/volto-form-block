3ae45995bef9ef11819d76b2ca1d7316eb79e24e
nzambello <nzambello@protonmail.com>
2021-08-07 16:08:15 +0200

    feat: add hcaptcha integration

--- src/components/FormView.jsx
+++ src/components/FormView.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { useIntl, defineMessages } from 'react-intl';
+import HCaptcha from '@hcaptcha/react-hcaptcha';
 import { GoogleReCaptcha } from 'react-google-recaptcha-v3';
 import {
   Segment,
@@ -46,19 +47,20 @@ const FormView = ({
 }) => {
   const intl = useIntl();
 
-  const [loadedRecaptcha, setLoadedRecaptcha] = useState(null);
+  const [loadedCaptcha, setLoadedCaptcha] = useState(null);
   let validToken = '';
   const onVerifyCaptcha = useCallback(
     (token) => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
+      console.log(token);
       validToken = token;
     },
     [validToken],
   );
 
   useEffect(() => {
-    setLoadedRecaptcha(true);
-  }, [loadedRecaptcha]);
+    setLoadedCaptcha(true);
+  }, [loadedCaptcha]);
 
   const isValidField = (field) => {
     return formErrors?.indexOf(field) < 0;
@@ -93,99 +95,107 @@ const FormView = ({
               </Button>
             </Message>
           ) : (
-            <Form
-              loading={formState.loading}
-              onSubmit={onSubmit}
-              autoComplete="off"
-              method="post"
-            >
-              <Grid columns={1} padded="vertically">
-                {data.static_fields?.map((field) => (
-                  <Grid.Row key={field.field_id} className="static-field">
-                    <Grid.Column>
-                      <Field
-                        {...field}
-                        field_type={field.field_type || 'text'}
-                        name={field.label}
-                        value={field.value}
-                        onChange={() => {}}
-                        disabled
-                        valid
-                        formHasErrors={formErrors.length > 0}
-                      />
-                    </Grid.Column>
-                  </Grid.Row>
-                ))}
-                {data.subblocks?.map((subblock, index) => {
-                  let name = getFieldName(subblock.label);
-                  return (
-                    <Grid.Row key={'row' + index}>
-                      <Grid.Column>
-                        <Field
-                          {...subblock}
-                          name={name}
-                          onChange={(field, value) =>
-                            onChangeFormData(
-                              subblock.id,
-                              field,
-                              value,
-                              subblock.label,
-                            )
+                <Form
+                  loading={formState.loading}
+                  onSubmit={onSubmit}
+                  autoComplete="off"
+                  method="post"
+                >
+                  <Grid columns={1} padded="vertically">
+                    {data.static_fields?.map((field) => (
+                      <Grid.Row key={field.field_id} className="static-field">
+                        <Grid.Column>
+                          <Field
+                            {...field}
+                            field_type={field.field_type || 'text'}
+                            name={field.label}
+                            value={field.value}
+                            onChange={() => { }}
+                            disabled
+                            valid
+                            formHasErrors={formErrors.length > 0}
+                          />
+                        </Grid.Column>
+                      </Grid.Row>
+                    ))}
+                    {data.subblocks?.map((subblock, index) => {
+                      let name = getFieldName(subblock.label);
+                      return (
+                        <Grid.Row key={'row' + index}>
+                          <Grid.Column>
+                            <Field
+                              {...subblock}
+                              name={name}
+                              onChange={(field, value) =>
+                                onChangeFormData(
+                                  subblock.id,
+                                  field,
+                                  value,
+                                  subblock.label,
+                                )
+                              }
+                              value={formData[name]?.value}
+                              valid={isValidField(name)}
+                            />
+                          </Grid.Column>
+                        </Grid.Row>
+                      );
+                    })}
+
+                    {process.env.RAZZLE_RECAPTCHA_KEY && (
+                      <Grid.Row>
+                        <Grid.Column>
+                          <GoogleReCaptcha onVerify={onVerifyCaptcha} />
+                        </Grid.Column>
+                      </Grid.Row>
+                    )}
+
+                    {process.env.RAZZLE_HCAPTCHA_KEY && (
+                      <Grid.Row>
+                        <Grid.Column>
+                          <HCaptcha sitekey={process.env.RAZZLE_HCAPTCHA_KEY} onVerify={onVerifyCaptcha} size="invisible" />
+                        </Grid.Column>
+                      </Grid.Row>
+                    )}
+
+                    {formErrors.length > 0 && (
+                      <Message error role="alert">
+                        <Message.Header as="h4">
+                          {intl.formatMessage(messages.error)}
+                        </Message.Header>
+                        <p>{intl.formatMessage(messages.empty_values)}</p>
+                      </Message>
+                    )}
+
+                    <Grid.Row centered style={{ paddingTop: '3rem' }}>
+                      <Grid.Column textAlign="center">
+                        <Button
+                          primary
+                          type="submit"
+                          disabled={
+                            (!loadedCaptcha &&
+                              (process.env.RAZZLE_RECAPTCHA_KEY || process.env.RAZZLE_HCAPTCHA_KEY)) ||
+                            formState.loading
                           }
-                          value={formData[name]?.value}
-                          valid={isValidField(name)}
-                        />
+                        >
+                          {data.submit_label ||
+                            intl.formatMessage(messages.default_submit_label)}
+
+                          {formState.loading && (
+                            <Progress
+                              role="progressbar"
+                              percent={100}
+                              active
+                              success={false}
+                              color="grey"
+                            />
+                          )}
+                        </Button>
                       </Grid.Column>
                     </Grid.Row>
-                  );
-                })}
-
-                {process.env.RAZZLE_RECAPTCHA_KEY && (
-                  <Grid.Row>
-                    <Grid.Column>
-                      <GoogleReCaptcha onVerify={onVerifyCaptcha} />
-                    </Grid.Column>
-                  </Grid.Row>
-                )}
-
-                {formErrors.length > 0 && (
-                  <Message error role="alert">
-                    <Message.Header as="h4">
-                      {intl.formatMessage(messages.error)}
-                    </Message.Header>
-                    <p>{intl.formatMessage(messages.empty_values)}</p>
-                  </Message>
-                )}
-
-                <Grid.Row centered style={{ paddingTop: '3rem' }}>
-                  <Grid.Column textAlign="center">
-                    <Button
-                      primary
-                      type="submit"
-                      disabled={
-                        (!loadedRecaptcha &&
-                          process.env.RAZZLE_RECAPTCHA_KEY) ||
-                        formState.loading
-                      }
-                    >
-                      {data.submit_label ||
-                        intl.formatMessage(messages.default_submit_label)}
-
-                      {formState.loading && (
-                        <Progress
-                          role="progressbar"
-                          percent={100}
-                          active
-                          success={false}
-                          color="grey"
-                        />
-                      )}
-                    </Button>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Form>
-          )}
+                  </Grid>
+                </Form>
+              )}
         </Segment>
       </div>
     </div>
