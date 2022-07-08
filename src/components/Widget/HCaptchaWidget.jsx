cb3084226581c9465c8f805256a272092f145eec
mamico <mauro.amico@gmail.com>
2022-07-08 19:34:18 +0200

    hcaptcha info translated

--- src/components/Widget/HCaptchaWidget.jsx
+++ src/components/Widget/HCaptchaWidget.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import { injectLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
+import { defineMessages, useIntl } from 'react-intl';
+
+const messages = defineMessages({
+  invisibleCaptcha: {
+    id: 'HCaptchaInvisibleInfo',
+    defaultMessage:
+      'This site is protected by hCaptcha and its ' +
+      '<a href="https://www.hcaptcha.com/privacy">Privacy Policy</a> and ' +
+      '<a href="https://www.hcaptcha.com/terms">Terms of Service</a> apply.',
+  },
+});
 
 const HCaptchaWidget = ({
   captchaRef,
@@ -10,6 +21,7 @@ const HCaptchaWidget = ({
   HCaptcha: hcaptchalib,
 }) => {
   const HCaptchaComponent = hcaptchalib.default;
+  const intl = useIntl();
 
   const onVerify = (token) => {
     captchaToken.current = token;
@@ -36,9 +48,11 @@ const HCaptchaWidget = ({
             onExpire={onExpire}
             size={size}
           />
-          This site is protected by hCaptcha and its
-          <a href="https://www.hcaptcha.com/privacy">Privacy Policy</a> and
-          <a href="https://www.hcaptcha.com/terms">Terms of Service</a> apply.
+          <div
+            dangerouslySetInnerHTML={{
+              __html: intl.formatMessage(messages.invisibleCaptcha),
+            }}
+          />
         </Grid.Column>
       </Grid.Row>
     );
