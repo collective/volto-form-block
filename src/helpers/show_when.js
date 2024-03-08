9ac3d2578023efa1017cd8aeafb1e7a1b1e57d5c
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-03-08 00:33:02 +0000

    Fix show when

--- src/helpers/show_when.js
+++ src/helpers/show_when.js
@@ -1,6 +1,16 @@
 const always = () => true;
-const value_is = ({ value, target_value }) => value === target_value;
-const value_is_not = ({ value, target_value }) => value !== target_value;
+const value_is = ({ value, target_value }) => {
+  if (Array.isArray(target_value)) {
+    return target_value.includes(value);
+  }
+  return value === target_value;
+};
+const value_is_not = ({ value, target_value }) => {
+  if (Array.isArray(target_value)) {
+    return !target_value.includes(value);
+  }
+  return value !== target_value;
+};
 
 export const showWhenValidator = {
   '': always,
