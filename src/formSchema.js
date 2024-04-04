36e4d837168d4c9485a437d4f82adda2a0890307
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-04-04 17:43:49 +0100

    Fix bad merge

--- src/formSchema.js
+++ src/formSchema.js
@@ -29,14 +29,29 @@ const messages = defineMessages({
     id: 'form_submit_label',
     defaultMessage: 'Submit button label',
   },
+  show_cancel: {
+    id: 'form_show_cancel',
+    defaultMessage: 'Show cancel button',
+  },
+  cancel_label: {
+    id: 'form_cancel_label',
+    defaultMessage: 'Cancel button label',
+  },
   captcha: {
     id: 'captcha',
     defaultMessage: 'Captcha provider',
   },
-
   store: {
     id: 'form_save_persistent_data',
     defaultMessage: 'Store compiled data',
+  },
+  remove_data_after_days: {
+    id: 'form_remove_data_after_days',
+    defaultMessage: 'Data wipe',
+  },
+  remove_data_after_days_helptext: {
+    id: 'form_remove_data_after_days_helptext',
+    defaultMessage: 'Number of days after which, the data should be deleted',
   },
   attachmentSendEmail: {
     id: 'form_attachment_send_email_info_text',
@@ -45,6 +60,19 @@ const messages = defineMessages({
   send: {
     id: 'form_send_email',
     defaultMessage: 'Send email to recipient',
+  },
+  send_message: {
+    id: 'form_send_message',
+    defaultMessage: 'Message of sending confirmed',
+  },
+  send_message_helptext: {
+    id: 'form_send_message_helptext',
+    defaultMessage:
+      'You can add the value of a filled field in the form by inserting its ID between curly brackets preceded by $, example: ${field_id}; you can add also html elements such as links <a>, new line <br />, bold <b> and italic <i> formatting.',
+  },
+  manage_data: {
+    id: 'form_manage_data',
+    defaultMessage: 'Manage data',
   },
   attachXml: {
     id: 'form_attach_xml',
@@ -56,8 +84,14 @@ const messages = defineMessages({
   },
 });
 
-export default (formData) => {
+const Schema = (data) => {
   var intl = useIntl();
+
+  let conditional_required = [];
+  if (!data.store && !data.send) {
+    conditional_required.push('store');
+    conditional_required.push('send');
+  }
 
   const fieldsets = [
     {
@@ -70,10 +104,15 @@ export default (formData) => {
         'default_from',
         'default_subject',
         'submit_label',
+        'show_cancel',
+        ...(data?.show_cancel ? ['cancel_label'] : []),
         'captcha',
-        'store',
-        'send',
       ],
+    },
+    {
+      id: 'manage_data',
+      title: intl.formatMessage(messages.manage_data),
+      fields: ['store', 'remove_data_after_days', 'send', 'send_message'],
     },
   ];
 
@@ -116,6 +155,14 @@ export default (formData) => {
       submit_label: {
         title: intl.formatMessage(messages.submit_label),
       },
+      show_cancel: {
+        type: 'boolean',
+        title: intl.formatMessage(messages.show_cancel),
+        default: false,
+      },
+      cancel_label: {
+        title: intl.formatMessage(messages.cancel_label),
+      },
       captcha: {
         title: intl.formatMessage(messages.captcha),
         type: 'array',
@@ -126,11 +173,16 @@ export default (formData) => {
       store: {
         type: 'boolean',
         title: intl.formatMessage(messages.store),
-        description: intl.formatMessage(messages.attachmentSendEmail),
       },
       send: {
         type: 'boolean',
         title: intl.formatMessage(messages.send),
+        description: intl.formatMessage(messages.attachmentSendEmail),
+      },
+      send_message: {
+        title: intl.formatMessage(messages.send_message),
+        type: 'textarea',
+        description: intl.formatMessage(messages.send_message_helptext),
       },
       attachXml: {
         type: 'boolean',
@@ -149,3 +201,5 @@ export default (formData) => {
     required: ['default_to', 'default_from', 'default_subject'],
   };
 };
+
+export default Schema;
