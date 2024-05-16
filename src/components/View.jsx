96d8a92d5148c21ba432fd7405958cdd5f0831e8
Maurits van Rees <maurits@vanrees.org>
2024-05-16 11:46:07 +0200

    fix: error on form submit: Objects are not valid as a React child.
    Fixes https://github.com/collective/volto-form-block/issues/96

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -295,10 +295,10 @@ const View = ({ data, id, path }) => {
     if (submitResults?.loaded) {
       setFormState({
         type: FORM_STATES.success,
-        result: {
-          message: intl.formatMessage(messages.formSubmitted),
-          ...submitResults.result,
-        },
+        result: [
+          intl.formatMessage(messages.formSubmitted),
+          submitResults.result
+        ]
       });
       captcha.reset();
       const formItem = document.getElementById(formid);
