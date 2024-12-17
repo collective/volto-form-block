477f0202cbc8d6bf4e3f5c04efff069655a4b1be
Rob Gietema <rob.gietema@gmail.com>
2024-12-17 11:21:42 -0700

    Add mail_templates vocabulary.

--- backend/src/collective/volto/formsupport/captcha/vocabularies.py
+++ backend/src/collective/volto/formsupport/captcha/vocabularies.py
@@ -1,4 +1,5 @@
 from ..interfaces import ICaptchaSupport
+from plone import api
 from zope.component import getAdapters
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
@@ -13,3 +14,11 @@ def captcha_providers_vocabulary_factory(context):
         if adapter.isEnabled():
             terms.append(SimpleTerm(value=name, token=name, title=adapter.name))
     return SimpleVocabulary(terms)
+
+
+@provider(IVocabularyFactory)
+def mail_templates_vocabulary_factory(context):
+    name = "schemaform.mail_templates"
+    registry_record_value = api.portal.get_registry_record(name)
+    items = list(registry_record_value.keys())
+    return SimpleVocabulary.fromItems([[item, item, item] for item in items])
