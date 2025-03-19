72e8b0f4ace9e06a352a794b37e0049f248015a3
Sara Bianchi <sonia_sara@live.it>
2025-03-19 17:23:24 +0100

    chore: updated field input values with different addresee

--- src/components/FieldTypeSchemaExtenders/SelectionSchemaExtender.js
+++ src/components/FieldTypeSchemaExtenders/SelectionSchemaExtender.js
@@ -4,6 +4,11 @@ const messages = defineMessages({
     id: 'form_field_input_values',
     defaultMessage: 'Possible values',
   },
+  field_input_values_description: {
+    id: 'form_field_input_values_description',
+    defaultMessage:
+      'You can create value pairs separated by a "|" symbol to add a different addressee based on your choice, e.g. "Value | email.recipient@gmail.com"', // È possibile creare coppie di valori divisi da simbolo "|" per aggiungere un destinatario diverso in base alla scelta effettuata, es. "Valore | email.destinatario@gmail.com"
+  },
 });
 
 export const SelectionSchemaExtender = (intl) => {
@@ -12,6 +17,9 @@ export const SelectionSchemaExtender = (intl) => {
     properties: {
       input_values: {
         title: intl.formatMessage(messages.field_input_values),
+        description: intl.formatMessage(
+          messages.field_input_values_description,
+        ),
         type: 'array',
         creatable: true,
       },
