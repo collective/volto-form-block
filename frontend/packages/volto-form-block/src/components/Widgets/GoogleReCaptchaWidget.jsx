ed5409b7115b4a70d301079775b152d235220ff9
Rob Gietema <rob.gietema@gmail.com>
2025-03-24 15:45:55 +0200

    Rewrite recaptcha widget.

--- frontend/packages/volto-form-block/src/components/Widgets/GoogleReCaptchaWidget.jsx
+++ frontend/packages/volto-form-block/src/components/Widgets/GoogleReCaptchaWidget.jsx
@@ -1,28 +1,43 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { injectLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
 import { useIntl } from 'react-intl';
 
-const GoogleReCaptchaWidget = (props) => {
-  const { captcha_props, GoogleReCaptcha: recaptchalib, id, onChange } = props;
+const ReCaptchaComponent = (props) => {
+  const { GoogleReCaptcha: recaptchalib, id, onChange } = props;
 
-  const { GoogleReCaptchaProvider, GoogleReCaptcha } = recaptchalib;
+  const { useGoogleReCaptcha } = recaptchalib;
+
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  const handleReCaptchaVerify = useCallback(async () => {
+    if (!executeRecaptcha) {
+      return;
+    }
+
+    const token = await executeRecaptcha();
+    onChange(id, token);
+  }, [executeRecaptcha]);
+
+  useEffect(() => {
+    handleReCaptchaVerify();
+  }, [handleReCaptchaVerify]);
+
+  return null;
+};
+
+const GoogleReCaptchaWidget = (props) => {
   const intl = useIntl();
-  const [submitted, setSubmitted] = useState(false);
+
+  const { captcha_props, GoogleReCaptcha: recaptchalib } = props;
+  const { GoogleReCaptchaProvider } = recaptchalib;
 
   return (
-    !submitted && (
-      <GoogleReCaptchaProvider
-        reCaptchaKey={captcha_props.public_key}
-        language={intl.locale ?? 'en'}
-      >
-        <GoogleReCaptcha
-          onVerify={(token) => {
-            setSubmitted(true);
-            onChange(id, token);
-          }}
-        />
-      </GoogleReCaptchaProvider>
-    )
+    <GoogleReCaptchaProvider
+      reCaptchaKey={captcha_props.public_key}
+      language={intl.locale ?? 'en'}
+    >
+      <ReCaptchaComponent {...props} />
+    </GoogleReCaptchaProvider>
   );
 };
 
