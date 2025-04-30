3b9413ce30821138ac39e74cce275d6873377a7e
Sara Bianchi <sonia_sara@live.it>
2025-04-30 17:33:51 +0200

    fix: added useEffect import

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Segment, Accordion, Form, Grid } from 'semantic-ui-react';
 import { defineMessages, useIntl, FormattedMessage } from 'react-intl';
