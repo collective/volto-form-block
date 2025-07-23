bde1a1a441d2bf686e6fb92b4ab78d9c300384a1
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-07-23 09:42:22 +0200

    Update Volto and linting

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const projectRootPath = __dirname;
-const AddonConfigurationRegistry = require('@plone/registry/src/addon-registry');
+const { AddonRegistry } = require('@plone/registry/addon-registry');
 
 let coreLocation;
 if (fs.existsSync(`${projectRootPath}/core`))
@@ -8,9 +8,7 @@ if (fs.existsSync(`${projectRootPath}/core`))
 else if (fs.existsSync(`${projectRootPath}/../../core`))
   coreLocation = `${projectRootPath}/../../core`;
 
-const registry = new AddonConfigurationRegistry(
-  `${coreLocation}/packages/volto`,
-);
+const { registry } = AddonRegistry.init(`${coreLocation}/packages/volto`);
 
 // Extends ESlint configuration for adding the aliases to `src` directories in Volto addons
 const addonAliases = Object.keys(registry.packages).map((o) => [
@@ -22,6 +20,27 @@ module.exports = {
   extends: `${coreLocation}/packages/volto/.eslintrc`,
   rules: {
     'import/no-unresolved': 1,
+    'import/named': 'error',
+    'react/jsx-filename-extension': 'error',
+    'no-restricted-imports': [
+      'error',
+      {
+        name: '@plone/volto/components',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/helpers',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+      {
+        name: '@plone/volto/actions',
+        message:
+          'Importing from barrel files is not allowed. Please use direct imports of the modules instead.',
+      },
+    ],
+    'react/jsx-key': [2, { checkFragmentShorthand: true }],
   },
   settings: {
     'import/resolver': {
@@ -32,7 +51,7 @@ module.exports = {
           ['@plone/registry', `${coreLocation}/packages/registry/src`],
           [
             'volto-form-block',
-            './packages/volto-form-block/src',
+            `${projectRootPath}/packages/volto-form-block/src`,
           ],
           ...addonAliases,
         ],
